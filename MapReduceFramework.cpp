9112146a1eae81acec413d26a363c24ed934738c
rz121 <ramon.zerem@mail.huji.ac.il>
2023-05-21 16:32:58 +0300

    Finish sort by the cuorect alghorithem

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -23,13 +23,18 @@ typedef struct {
     float percentage;
 } JobState;
 
+
+class JobContext;
+
 struct ThreadContext {
+    int ID;
     pthread_t thisThread;
     const MapReduceClient* mapReduceClient;
     int startIndex, endIndex;
     const InputVec* inputVec;
     IntermediateVec* threadIntermediateVec;
     std::atomic<int>* atomic_counter;
+    JobContext *globalJobContext;
     //OutputVec* threadOutputVec;
 };
 
@@ -37,10 +42,11 @@ struct JobContext {
     JobState* myState;
     ThreadContext *threadsContext;
     pthread_mutex_t* pthreadMutex;
-    sem_t* sem;
+    sem_t sem;
     std::atomic<int>* atomic_counter;
     IntermediateVec* intermediateVec;
-    std::mutex insertIntermediateVecsMutex;
+    int numOfThreads;
+    //std::mutex insertIntermediateVecsMutex;
 };
 
 bool compareIntermediateVec(const IntermediatePair& a, const IntermediatePair& b){
@@ -48,9 +54,15 @@ bool compareIntermediateVec(const IntermediatePair& a, const IntermediatePair& b
 }
 
 void sortIntermediateVec(ThreadContext* threadContext){
-    std::sort(threadContext->threadIntermediateVec->begin(), threadContext->threadIntermediateVec->end(), compareIntermediateVec);
+    std::sort(threadContext->threadIntermediateVec->begin(),
+              threadContext->threadIntermediateVec->end(),
+              compareIntermediateVec);
 }
-void insertIntermediateVecs(JobContext* jobContext, ThreadContext* threadContext){
+
+
+/*
+void insertIntermediateVecs(JobContext* jobContext,
+                            ThreadContext* threadContext){
     std::lock_guard<std::mutex> lock(jobContext->insertIntermediateVecsMutex);
     auto lastIndex = jobContext->intermediateVec->begin();
     for (int i = 0; i < threadContext->threadIntermediateVec->size(); i++){
@@ -62,6 +74,45 @@ void insertIntermediateVecs(JobContext* jobContext, ThreadContext* threadContext
         lastIndex = it; //TODO make sure its logical
     }
     //std::lock_guard<std::mutex> unlock(jobContext->insertIntermediateVecsMutex); //TODO check if neccasry
+}
+*/
+K2* findLargestKey(JobContext* jobContext) {
+    ThreadContext* tc = (ThreadContext*) (jobContext->threadsContext);
+    K2* large = nullptr;
+    for (int i = 0; i < jobContext->numOfThreads; i++){
+        ThreadContext* tc = (ThreadContext*) (jobContext->threadsContext + i); //TODO check
+        if (tc->threadIntermediateVec->empty())
+            continue;
+        IntermediatePair *temp = &tc->threadIntermediateVec->back();
+        if (large == nullptr || large < temp->first){
+            large = temp->first;
+        }
+    }
+    return large;
+}
+
+void insertIntermediateVecs(JobContext* jobContext, ThreadContext* threadContext){
+    if (threadContext->ID != 0){
+        sem_wait(&(jobContext->sem));
+    }
+    else{
+        while (true){
+            K2 *curLargestKey = findLargestKey(jobContext);
+            if (curLargestKey == nullptr){
+                break;
+            }
+            for (int i = 0; i < jobContext->numOfThreads; i++){
+                ThreadContext* tc = (ThreadContext*) (jobContext->threadsContext + i); //TODO check
+                IntermediatePair temp = tc->threadIntermediateVec->back();
+                if (!(temp.first < curLargestKey) &&
+                        !(curLargestKey < temp.first)){
+                    jobContext->intermediateVec->insert(jobContext->intermediateVec->begin(), temp);
+                    tc->threadIntermediateVec->pop_back();
+                }
+            }
+        }
+        sem_post(&(jobContext->sem));
+    }
 }
 
 void emit2 (K2* key, V2* value, void* context) {
@@ -83,6 +134,7 @@ void* mapWraper(void* arg){
                                             threadContext);
     }
     sortIntermediateVec(threadContext);
+    insertIntermediateVecs(threadContext->globalJobContext, threadContext);
     //pthread_mutex_unlock(&mtx);
 }
 
@@ -90,17 +142,20 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
                             const InputVec& inputVec, OutputVec& outputVec,
                             int multiThreadLevel) {
     std::atomic<int> atomic_counter(0);
-    ThreadContext* allThreadsContext = new ThreadContext[multiThreadLevel];
+    ThreadContext allThreadsContext[multiThreadLevel];
     pthread_t threads[multiThreadLevel];
     JobContext *jobContext = new JobContext;
     jobContext->threadsContext = allThreadsContext;
     jobContext->myState = new JobState[multiThreadLevel];
     jobContext->atomic_counter = &atomic_counter;
     jobContext->intermediateVec = new IntermediateVec;
+    jobContext->numOfThreads = multiThreadLevel;
     int pairsForThread = inputVec.size()/multiThreadLevel;
     for (int i = 0; i < multiThreadLevel; i++) {
         jobContext->myState[i].stage = UNDEFINED_STAGE;
         //ThreadContext* threadContext = new ThreadContext;
+        allThreadsContext[i].globalJobContext = jobContext;
+        allThreadsContext[i].ID = i;
         allThreadsContext[i].thisThread = threads[i];
         allThreadsContext[i].mapReduceClient = &client;
         allThreadsContext[i].inputVec = &inputVec;
@@ -115,6 +170,10 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
         }
         pthread_create(threads + i, NULL, mapWraper, &allThreadsContext[i]);
     }
+    for (int i = 0; i < multiThreadLevel; i++){
+        pthread_join(allThreadsContext[i].thisThread, NULL);
+    }
+
     //
     return static_cast<JobHandle>(jobContext);
 }
